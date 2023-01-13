1e7175d619793e155a239dfefa22bd6ac9efba27
Ediliani Kumquel <kumquelediliani@gmail.com>
2023-01-13 20:55:48 +0100

    ECKG añadir la funcion para crear las tareas en la lista

--- script.js
+++ script.js
@@ -16,4 +16,15 @@ const setDate = () => {
     dateYear.textContent = date.toLocaleString('es', {year: 'numeric'});
 };
 
+const addNewTask = event =>{
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if (!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+}
+
 setDate();
